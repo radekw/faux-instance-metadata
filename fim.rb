3d8cf0832ff0aba443a94b3473b2da791f30ba75
Radek Wierzbicki <radek@opsline.com>
2014-04-06 22:18:19 -0400

    initial import

---
+++ fim.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'sinatra'
+
+versions = [
+  'latest'
+]
+
+def create_route(path, value)
+  puts "Creating route #{path}"
+  if value.class == String
+    get path do 
+      value
+    end
+  elsif value.class == Hash
+    create_route("#{path}", "")
+    create_route("#{path}/", value.keys.join("\n"))
+    value.each do |k, v|
+      create_route("#{path}/#{k}", v)
+    end
+  end
+end
+
+md = JSON.parse(File.read('fim.json'))
+
+versions.each do |version|
+  create_route("/#{version}/meta-data", "")
+  create_route("/#{version}/meta-data/", md.keys.join("\n"))
+end
+
+md.each do |k, v|
+  versions.each do |version|
+    create_route("/#{version}/meta-data/#{k}", v)
+  end
+end
